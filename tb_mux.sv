3c14a5f670f17c635729ef28b564abdb9e39052a
Davi Lima Pinheiro <davipinheiro1266@gmail.com>
2026-03-07 16:39:37 -0300

    Refactor tb_mux module and update mux instantiation

--- tb_mux.sv
+++ tb_mux.sv
@@ -1,18 +1,20 @@
 `timescale 1ns/1ps
 
-module tb_mux;
-logic [31:0] e1 = 32'd2000;
-logic [31:0] e2 = 32'd1000;
-logic [31:0] e3 = 32'd555;
-logic [31:0] e4 = 32'd234;
+module tb_mux
+(
+logic [31:0] a = 32'd2000;
+logic [31:0] b = 32'd1000;
+logic [31:0] c = 32'd555;
+logic [31:0] d = 32'd234;
 logic [2:0] selecao;
 logic [31:0] muxOut;
+);
+   
+mux dut(.f(muxOut),.e1(a),.e2(b),.e3(c),.e4(d),.sel(selecao));
 
-   mux dut(.saida(muxOut), .a(e1), .b(e2), .c(e3), .d(e4), sel.(selecao));
+initial begin
+$monitor($time," a=%d | b=%d | c=%d | d=%d | sel=%b | muxOut=%d",a, b, c, d, selecao, muxOut);
+for(selecao = 0; selecao < 4; selecao++) #10;
+#10 $stop;
 
-   initial begin
-     $monitor($time,"a = %b | b = %b | c = %b | d = %d | sel = %b | muxOut = %b", e1, e2, e3, e4, selecao, muxOut);
-     for(sel = 0; sel != 3'b111; sel++) #10;     
-     #10 $stop;
-
-   end
+endmodule
