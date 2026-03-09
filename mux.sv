8ba6bfd8ae20da6ca28b11dca0b27fa219b5e423
Davi Lima Pinheiro <davipinheiro1266@gmail.com>
2026-03-08 21:59:56 -0300

    Refactor mux module syntax and formatting

--- mux.sv
+++ mux.sv
@@ -1,19 +1,16 @@
 module mux
 (
-input logic [31:0] e1,e2,e3,e4,
-input logic [2:0] sel,
-output logic [31:0] f
+    input  logic [31:0] e1, e2, e3, e4,
+    input  logic [2:0]  sel,
+    output logic [31:0] f
 );
-
 always_comb begin
-
-case (sel)
-3'b000 : f = e1;
-3'b001 : f = e2;
-3'b010 : f = e3;
-3'b011 : f = e4;
-default : f = 32'd0;
-endcase
+    case (sel)
+        3'b000  : f = e1;
+        3'b001  : f = e2;
+        3'b010  : f = e3;
+        3'b011  : f = e4;
+        default : f = 32'd0;
+    endcase
 end
-
 endmodule
