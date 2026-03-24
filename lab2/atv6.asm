96b75f457070146e40497d2df8e7eef32c412313
Davi Lima Pinheiro <davipinheiro1266@gmail.com>
2026-03-23 22:16:11 -0300

    Add assembly code for loop and byte manipulation

---
+++ lab2/atv6.asm
@@ -0,0 +1,26 @@
+addi x11, x0, 1
+addi x12, x0, 32 
+addi x13, x0, 1
+sb x11, 1029(x0)
+
+loop:
+	beq x11, x12, loop
+	lb x10, 1026(x0)
+	andi x10, x10, 1
+	beq x10, x13, on
+	jal x0, loop
+
+on:
+	slli x11, x11, 1
+	sb x11, 1029(x0)
+	jal x0, loop
+
+off:
+	lb x10, 65(x0)
+	sb x0, 1029(x0)
+	jal x0, loop
+
+halt 
+
+HIGH: .byte 1
+LOW:  .byte 0
