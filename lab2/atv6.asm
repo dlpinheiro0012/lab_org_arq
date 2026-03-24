c570d41d8e94def51d3247026c5dcd4f93e80db3
Davi Lima Pinheiro <davipinheiro1266@gmail.com>
2026-03-23 22:38:20 -0300

    Fix loop condition in atv6.asm

--- lab2/atv6.asm
+++ lab2/atv6.asm
@@ -1,13 +1,10 @@
-# Este código foi testado em outro PC e funcionou, mas no meu pessoal não funcionou; eu reiniciei o compsim 7 vezes
-# mas ainda assim não deu a resposta desejada. 
-
 addi x11, x0, 1
 addi x12, x0, 32 
 addi x13, x0, 1
 sb x11, 1029(x0)
-
+ 
 loop:
-	beq x11, x12, loop
+	beq x11, x12, off 
 	lb x10, 1026(x0)
 	andi x10, x10, 1
 	beq x10, x13, on
